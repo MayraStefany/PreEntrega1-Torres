38861246e384155dbd812d88832a42e87d04334e
Mayra Torres <isemayratorres18@gmail.com>
2024-08-26 19:41:34 -0500

    Formato - Coder prueba Formato

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,8 @@
-import { Flex,Text } from "@chakra-ui/react"
+import { Flex, Text, Badge } from "@chakra-ui/react"
 import { FaShoppingCart } from "react-icons/fa";
 
 const CartWidget = () => {
-    return(
+    return (
         <Flex alignItems={"center"}>
             <FaShoppingCart size={24} style={{ marginRight: '8px' }} />
             <Text>1</Text>
